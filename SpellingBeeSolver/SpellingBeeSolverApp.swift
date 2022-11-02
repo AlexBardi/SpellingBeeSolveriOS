2b80d7fd02f3bb626d1d058d51b678af5a831ad7
Alex Bardi <alex.bardi@hey.com>
2022-11-02 07:29:56 -0500

    Initial Commit

---
+++ SpellingBeeSolver/SpellingBeeSolverApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpellingBeeSolverApp.swift
+//  SpellingBeeSolver
+//
+//  Created by Alex Bardi on 11/2/22.
+//
+
+import SwiftUI
+
+@main
+struct SpellingBeeSolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
